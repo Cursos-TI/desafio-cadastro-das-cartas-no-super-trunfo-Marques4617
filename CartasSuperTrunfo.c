538da9c79378322c0a08bbdec235679c488696e1
Marques4617 <pedrohmarquescarvalho@gmail.com>
2025-05-12 10:57:23 -0300

    Update CartasSuperTrunfo.c
    Atualizei do nível Avetureiro, para o nível Mestre.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -75,9 +75,11 @@ printf("\n");
 
 int densidade1 = populacao1 / area1;
 int capita1 = pib1 / populacao1;
+unsigned int super1 = populacao1 + area1 + pib1 + pontos1 + capita1 + (1.0/densidade1);
 
 int densidade2 = populacao2 / area2;
 int capita2 = pib2 / populacao2;
+unsigned int super2 = populacao2 + area2 + pib2 + pontos2 + capita2 + (1.0/densidade2);
 
 //Exibição de dados
 
@@ -91,6 +93,7 @@ printf("PIB: %.2f bilhoes de reais\n", pib1);
 printf("Numero de Pontos Turisticos: %d\n", pontos1);
 printf("Densidade Populacional: %.2f hab/km²\n", densidade1);
 printf("PIB per Capita: %.2f reais\n", capita1);
+printf("Super Poder: %u\n", &super1);
 
 printf("\n");
 
@@ -104,6 +107,53 @@ printf("PIB: %.2f bilhoes de reais\n", pib2);
 printf("Numero de Pontos Turisticos: %d\n", pontos2);
 printf("Densidade Populacional: %.2f hab/km²\n", densidade2);
 printf("PIB per Capita: %.2f reais\n", capita2);
+printf("Super Poder: %u\n", super2);
+printf("\n");
+
+// Comparações
+
+printf("Resultados das comparações:\n");
+
+if (populacao1 > populacao2)
+    printf("Populacao: Carta 1 venceu\n");
+else if (populacao2 > populacao1)
+    printf("Populacao: Carta 2 venceu\n");
+
+
+if (area1 > area2)
+    printf("Area: Carta 1 venceu\n");
+else if (area2 > area1)
+    printf("Area: Carta 2 venceu\n");
+
+
+if (pib1 > pib2)
+    printf("PIB: Carta 1 venceu\n");
+else if (pib2 > pib1)
+    printf("PIB: Carta 2 venceu\n");
+
+
+if (pontos1 > pontos2)
+    printf("Pontos Turisticos: Carta 1 venceu\n");
+else if (pontos2 > pontos1)
+    printf("Pontos Turisticos: Carta 2 venceu\n");
+
+
+if (densidade1 < densidade2)
+    printf("Densidade Populacional: Carta 1 venceu (menor densidade)\n");
+else if (densidade2 < densidade1)
+    printf("Densidade Populacional: Carta 2 venceu (menor densidade)\n");
+
+
+if (capita1 > capita2)
+    printf("PIB per Capita: Carta 1 venceu\n");
+else if (capita2 > capita1)
+    printf("PIB per Capita: Carta 2 venceu\n");
+
+
+if (super1 > super2)
+    printf("Super Poder: Carta 1 venceu\n");
+else if (super2 > super1)
+    printf("Super Poder: Carta 2 venceu\n");
 
 
 }
